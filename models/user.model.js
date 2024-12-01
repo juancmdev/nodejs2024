2719832105771b051e357946043e4f2ad323cd67
juancmdev <juancmorenoj@gmail.com>
2024-12-01 14:58:50 -0500

    update

---
+++ models/user.model.js
@@ -0,0 +1,15 @@
+import { db } from "../database/conection.database.js";
+
+const create = async ({ email, password, username }) => {
+  const query = {
+    text: `
+        INSERT INTO users (email, password, username)
+        VALUES ($1, $2, $3)
+        `,
+    values: [],
+  };
+};
+
+export const UserModel = {
+  create,
+};
